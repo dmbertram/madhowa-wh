af39961f88154fe1fdae9485aebf73f14d630368
dudley <dudley.bertram@gmail.com>
2017-08-05 09:28:53 -0400

    Added remaining warehouse tree build

--- makewh.py
+++ makewh.py
@@ -10,10 +10,16 @@ def main():
     rdr = csv.reader(inf)
     for line in rdr:
         aisle, row, sect = line[1].split("-")
-        print "aisle = %s, row = %s, sect = %s \n" % (aisle, row, sect)
         if aisle != cur_aisle:
             cur_aisle = aisle
-            print ">>>>>>  create new parent warehouose = " + cur_aisle
+            res = aisle + " - MA," + aisle + ",Madhowa Associates,1,All Warehouses - MA"
+            wrt.writerow(res.split(","))
+        if row != cur_row:
+            cur_row = row
+            res = row + " - MA," + row + ",Madhowa Associates,1," + cur_aisle + " - MA"
+            wrt.writerow(res.split(","))
+        res = sect + " - MA," + sect + ",Madhowa Associates,0," + cur_row + " - MA"
+        wrt.writerow(res.split(","))
     inf.close()
     outf.close()
 
